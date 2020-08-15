699bae4f23a4f50ec449f9a0d66bfe5b4b6809f4
karkarson <karkarsongit.@gmail.com>
2020-08-15 11:31:24 +0900

    init

---
+++ src/main/java/com/example/ppl/PplApplication.java
@@ -0,0 +1,13 @@
+package com.example.ppl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PplApplication.class, args);
+	}
+
+}
